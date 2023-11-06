40ffa633e1431c63e210afe466096564f4869577
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-06 19:19:27 -0300

    Add files via upload

---
+++ BottomSheet/BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Lucas Neves dos santos pompeu on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
